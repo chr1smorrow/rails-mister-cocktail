6bf27496255ffb8c588911de76e4f8227562e287
Chris Morrow <chrismorrow@blueyonder.co.uk>
2018-05-18 17:39:02 +0100

    blah

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,28 +1,25 @@
 class DosesController < ApplicationController
   before_action :set_dose, only: [:show, :destroy]
-  before_action :set_cocktail, only: [:create, :destroy]
+  before_action :set_cocktail, only: [:create, :new]
 
   def new
     @dose = Dose.new
-  end
-
-  def show
-
   end
 
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
+    cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(cocktail)
   end
 
   private
@@ -32,7 +29,7 @@ class DosesController < ApplicationController
   end
 
   def set_dose
-    @dose = Dose.find(params[:dose_id])
+    @dose = Dose.find(params[:id])
   end
 
   def dose_params
